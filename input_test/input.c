8518418e66845ff705bf88112a55e8afc9b622b8
Rocche <giacomorocchetti97@gmail.com>
2018-09-17 11:26:06 +0200

    Meccanismo di distinzione tra argomenti di linea di comando

--- input_test/input.c
+++ input_test/input.c
@@ -3,6 +3,45 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello!\n");
+    FILE *fp;
+    //controllo argomenti da linea di comando
+    while (*(++argv))
+    {
+        //se inizia con un '-', l'argomento è una option
+        if ( (*argv)[0] == '-' )
+        {
+            switch ((*argv)[1])  
+            {
+                default:
+                    printf("Unknown option -%c\n", (*argv)[1]);
+                    break;
+                case 'h':
+                    printf("--help\n");
+                    break;
+                case 'v':
+                    printf("--verbose\n");
+                    break;
+            }
+        }
+        //altrimenti è un parametro di input
+        else{
+            printf("Si è scelto di contare le occorrenze di %s\n\n", (*argv));
+            fp = fopen(*argv, "r");
+            //se non è stato trovato il file
+            if(fp == NULL){
+                printf("File non trovato.\n");
+                exit(1);
+            }
+            else{
+                char c = fgetc(fp);
+                while(c != EOF){
+                    printf("%c", c);
+                    c = fgetc(fp);
+                }
+            }
+            printf("\n");
+            fclose(fp);
+        }
+    }
     return 0;
 }
